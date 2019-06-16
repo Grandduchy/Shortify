ee6ac65e5bde45e5e91512d02b8a667ae0519a0a
Grandduchy <jchallenger1@hotmail.com>
2019-06-16 19:38:06 -0400

    Added and applied migrations

--- shorten/models.py
+++ shorten/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Url(models.Model):
+    url = models.CharField(max_length=300)
+    shortenUrl = models.CharField(max_length=50)
+    dateAdded = models.DateTimeField(auto_now_add=True)
+
