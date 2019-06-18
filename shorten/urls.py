fc458baf19c8ef791e132928aa8e621a2bd53db8
Grandduchy <jchallenger1@hotmail.com>
2019-06-18 13:14:27 -0400

    Added api methods

--- shorten/urls.py
+++ shorten/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="shortify-index"),
     path("url/<str:shortedUrl>/", views.url, name="shortedUrl"),
+    path("api/get", views.get, name="url-get"),
+    path("api/post", views.post, name="url-post"),
+    path("api/delete", views.delete, name="url-delete"),
 ]
--- shorten/views.py
+++ shorten/views.py
@@ -12,3 +12,21 @@ from .models import Url
 def url(request, shortedUrl):
     redirectUrl = get_object_or_404(Url, shortenUrl=shortedUrl)
     return HttpResponseRedirect(redirectUrl.url)
+
+
+# RESTful API for using the database with javascript
+# Would be better for it to a new view than this one
+
+def get(request):
+    print(request.GET)
+    return HttpResponse()
+
+
+def post(request):
+    print("POST REQUEST : ")
+    print(request.POST)
+    return HttpResponse()
+
+
+def delete(request):
+    pass
